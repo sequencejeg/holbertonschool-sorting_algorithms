c0fde98c2072c88600f51a91f80ab4d85bf6a94d
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-04-09 22:18:25 -0400

    Fix on ex 0 and 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,35 @@
+#include <stdio.h>
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - Sorts an array of integers in ascending order using
+ * the bubble sort algorithm.
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp;
+	int swapped;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
 			}
 		}
+
+		if (swapped == 0)
+			break;
+
+	}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,32 @@
+#include <stdio.h>
 #include "sort.h"
+
 /**
- * selection_sort - function that sorts an array of integers in ascending
- * order using the Selection sort algorithm
- * @size: size of the array
- * @array: list with numbers
+ * selection_sort - Sorts an array of integers in ascending order using
+ * the Selection sort algorithm.
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, index;
-	int tmp, swap, flag = 0;
+	size_t i, j, min_idx;
+	int temp;
 
-	if (array == NULL)
-		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		tmp = i;
-		flag = 0;
-		for (index = i + 1; index < size; index++)
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[tmp] > array[index])
-			{
-				tmp = index;
-				flag += 1;
-			}
+			if (array[j] < array[min_idx])
+				min_idx = j;
 		}
-		swap = array[i];
-		array[i] = array[tmp];
-		array[tmp] = swap;
-		if (flag != 0)
+
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
 			print_array(array, size);
+		}
 	}
 }
